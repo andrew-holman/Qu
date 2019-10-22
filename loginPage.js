0373e7c35ff098f45b85803834209701a1424f25
netId <netId@iastate.edu>
2019-10-22 13:18:34 -0500

    Merged into the development branch

---
+++ loginPage.js
@@ -0,0 +1,42 @@
+function signInUser(){
+    var email = document.getElementById('email').value
+    var emailTag = validateEmail(email)
+    var emailCheck = emailTag == ""
+
+    var password = document.getElementById('password').value
+    var passwordTag = validatePassword(password)
+    var passwordCheck = passwordTag == ""
+
+    var alertMessage = ""
+    alertMessage += emailCheck ? "" : emailTag +"\n"
+    alertMessage += passwordCheck ? "" : passwordTag
+    
+    if(alertMessage == ""){
+        document.getElementById('password').value = ""
+        document.getElementById('email').value = ""
+        window.alert("Successful")
+    }
+    else window.alert(alertMessage)
+}
+
+function validateEmail(givenEmail) {
+    if(givenEmail == "") return "Email cannot be empty!"
+    let alphanumeric = /^[A-za-z0-9.]+$/i;
+    atSymbolSplit = givenEmail.split('@');
+    if ((atSymbolSplit[0]).match(alphanumeric) && atSymbolSplit.length == 2) {
+        periodSplit = atSymbolSplit[1].split('.')
+        if ((periodSplit[0] + periodSplit[1]).match(alphanumeric) && periodSplit.length == 2) return "";
+    }
+    return "Invalid email, must be in the format abc@abc.abc"
+}
+
+function validatePassword(password){
+    let alpha = /[A-Za-z]/i;
+    let numer = /[0-9]/;
+    let special = /[!@#$%\^&*(),\.?;":{}|<>\']/;
+    if(password == "") return "Password cannot be empty!"
+    if(password.length < 8) return "Password must be at least 8 characters"
+    if(!(password.match(alpha) && password.match(numer) && password.match(special))) return "Password must contain alphabetic, numeric, and a special character"
+    return ""
+}
+
---
+++ navigation.js
@@ -0,0 +1,11 @@
+function main(){
+    window.location.href = "C:/Qu/mainPage.html"
+}
+
+function login(){
+    window.location.href = "C:/Qu/loginPage.html"
+}
+
+function signUp(){
+    window.location.href = "C:/Qu/signUpPage.html"
+}
