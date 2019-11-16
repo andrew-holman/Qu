ebe065ca9be96aa4369150572b392cbc4e4f5874
Andrew <aholman@iastate.edu>
2019-11-16 14:11:10 -0600

    Added query and class repositories. Lets front end create instances of a class and send/receive queries.

--- complete/src/main/java/com/example/accessingdatamysql/ClassRepository.java
+++ complete/src/main/java/com/example/accessingdatamysql/ClassRepository.java
@@ -1,8 +1,6 @@
 package com.example.accessingdatamysql;
 
-import com.example.accessingdatamysql.User;
 import org.springframework.data.repository.CrudRepository;
-import java.util.*;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
