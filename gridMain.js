14b48662f68495f1d107d8a2c2a0a52f25f4ad78
netId <netId@iastate.edu>
2019-12-10 22:53:01 -0600

    Fixing Grid page

--- gridMain.js
+++ gridMain.js
@@ -3,7 +3,7 @@ var email = sessionStorage.getItem("Email")
 var displayName = sessionStorage.getItem("DisplayName")
 var classId = sessionStorage.getItem("classID")
 console.log("CLass ID: " + classId);
-var isCreator = false//sessionStorage.getItem("creator") === "TRUE";
+var isCreator = true//sessionStorage.getItem("creator") === "TRUE";
 var className = sessionStorage.getItem("className")
 var completedQueries = []
 var webSocket;
@@ -15,11 +15,11 @@ document.addEventListener("DOMContentLoaded", function() {
     new agGrid.Grid(eGridDiv, gridOptions);
 });
 
-document.getElementById("addRow").style.visibility = isCreator ? "hidden" : "visible"
-document.getElementById("queryType").style.visibility = isCreator ? "hidden" : "visible"
-document.getElementById("selectType").style.visibility = isCreator ? "hidden" : "visible"
-document.getElementById("queryText").style.visibility = isCreator ? "hidden" : "visible"
-document.getElementById("queryMessage").style.visibility = isCreator ? "hidden" : "visible"
+document.getElementById("addRow").style.visibility = !isCreator ? "hidden" : "visible"
+document.getElementById("queryType").style.visibility = !isCreator ? "hidden" : "visible"
+document.getElementById("selectType").style.visibility = !isCreator ? "hidden" : "visible"
+document.getElementById("queryText").style.visibility = !isCreator ? "hidden" : "visible"
+document.getElementById("queryMessage").style.visibility = !isCreator ? "hidden" : "visible"
 document.getElementById("removeSelected").style.visibility = isCreator ? "visible" : "hidden"
 document.getElementById("complete").style.visibility = isCreator ? "visible" : "hidden"
 createConnection();
@@ -192,9 +192,11 @@ function onRemoveSelected(completed) {
             gridOptions.api.updateRowData({remove: selectedData});
         }
     }
+    console.log(rowData)
     gridOptions.api.updateRowData({remove: selectedData});
+    console.log(rowData)
     updateRowDataClient()
-    console.log(rowData + "HEllo")
+    console.log(rowData)
     console.log(rowData.length)
 }
 
@@ -229,6 +231,5 @@ function updateRowDataClient(){
 }
 
 function clearEntries(){
-    document.getElementById("queryTypeText").value = ""
     document.getElementById("queryMessage").value = ""
 }
